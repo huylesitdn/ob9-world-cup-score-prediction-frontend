64e8ff29dcbb100dfc6130896993922c83963844
huylesitdn <mrsjiodev@gmail.com>
2022-11-11 21:14:08 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import Home from "./pages/Home";
+
+const routers = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: "Not Found",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          {routers.map((router, k) => (
+            <Route {...router} key={k} />
+          ))}
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
