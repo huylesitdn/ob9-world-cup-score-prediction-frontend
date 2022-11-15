2a75527b1cc42eca65ddef738767243f59a481c6
huylesitdn <mrsjiodev@gmail.com>
2022-11-15 22:37:40 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const BACKEND_URL = process.env.REACT_APP_API_URL || "https://world-cup-s
 export const API_URL = process.env.REACT_APP_API_URL || "https://world-cup-score-prediction.herokuapp.com/api";
 // export const BACKEND_URL = process.env.REACT_APP_API_URL || "http://localhost:1337";
 // export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:1337/api";
-export const API_ACCESS_TOKEN = '5af17c8981a30192d08db1c59377cbcd6a1ace148b97019fb86e4099172408df30fdbb50695441ec171d0e4c8ae00b6b3a519a25c3cead3ee89b2a37c81f4ef7725e48946baafdf76a89b05fa8bf12d8e3f28cce53363689c2ebaad2d43a75636c9a1d4619f1eada19a5a8641957738ef8054fea805ff352e39c97badfeca51b'
+export const API_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY4NTI2NTIxLCJleHAiOjE3MzE2NDE3MjF9.e1-d4WI9ZxtDpA8SSRdMqWo2pokoTIFSFqEjI9gdC7I";
